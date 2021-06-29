006ebd608e6de3a26101e2b44b437c6ca9d20494
SafeTestFi <86659518+SafeTestFi@users.noreply.github.com>
2021-06-29 10:45:46 +0000

    init

---
+++ safebase-test/build/precache-manifest.f8eed65b25f41bced165c4ee9bf29dd0.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7b9f664e5b6e0ae166d5fed6b98f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ffbc7923eb25241da84",
+    "url": "/static/css/3.15a0e9ba.chunk.css"
+  },
+  {
+    "revision": "3eedbbab2e69f7c9a94b",
+    "url": "/static/css/main.2c05d58f.chunk.css"
+  },
+  {
+    "revision": "e097fac301c998cda405",
+    "url": "/static/js/0.c2a88194.chunk.js"
+  },
+  {
+    "revision": "d94896e7150ef85c7c8a",
+    "url": "/static/js/10.cdda95c0.chunk.js"
+  },
+  {
+    "revision": "2ffbc7923eb25241da84",
+    "url": "/static/js/3.d011ab22.chunk.js"
+  },
+  {
+    "revision": "01f18a54b4bb4c9a574e97e9ff54ca12",
+    "url": "/static/js/3.d011ab22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449fe46747ea41c15202",
+    "url": "/static/js/4.6db59623.chunk.js"
+  },
+  {
+    "revision": "3c8bc87d347048425458",
+    "url": "/static/js/5.7933f7ae.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/5.7933f7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56bfc9359de6ca923e15",
+    "url": "/static/js/6.b3dcd8b9.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.b3dcd8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c626e8008814f6addd8",
+    "url": "/static/js/7.87430bc1.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.87430bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c60f8e11b4c57bb1091",
+    "url": "/static/js/8.9d8662bc.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.9d8662bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cddb9f5907a102f2de",
+    "url": "/static/js/9.11ef6b41.chunk.js"
+  },
+  {
+    "revision": "3eedbbab2e69f7c9a94b",
+    "url": "/static/js/main.1529533a.chunk.js"
+  },
+  {
+    "revision": "e5c1a4ef5d15dfc60159",
+    "url": "/static/js/runtime-main.0211b993.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
